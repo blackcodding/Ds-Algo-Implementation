31965dd4430762fde751a295f27fedb93ab9286c
SUPRIYA BARAI <70096639+blackcodding@users.noreply.github.com>
2021-09-22 21:37:24 +0530

    Using cin.get() - in Character Array

---
+++ getline.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char sentence[1000];
+    cin.getline(sentence, 1000, '.'); // here in the cin.getline() it takes 3 argument first one is the array name, second one is the length and in case of third one if we dont put any thing it is taking \n as bu default bt we can change it by any condition where we need to stop taking the inputs and immediately show the output
+    cout<<sentence;
+    return 0;
+}
